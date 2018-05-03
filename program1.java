0dc7cf19fc0b7e5ed42b1c8b38becb9353e4f00b
AKSHAYGANDHI17 <akshaygandhi03@gmail.com>
2018-05-03 15:34:31 +0530

    file created program1.java

---
+++ program1.java
@@ -0,0 +1,14 @@
+class program1
+{
+  public static void main(String[] args) {
+    Scanner s= new Scanner(System.in);
+    int i,j,r,c;
+    System.out.println("enter row and column");
+    r=s.nextInt;
+    c=s.nextInt;
+    for (i=0;; ; ) {
+
+    }
+
+  }
+}
